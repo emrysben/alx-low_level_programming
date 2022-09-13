13c635ed1e5f56aed4a78829ae80bfe812adb7ff
emrysben <emyrsben32@gmail.com>
2022-09-13 21:01:05 +0000

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,5 @@
-#include <main.h>
+#include <stdio.h>
+#include "main.h"
 /**
  * - program that prints _putchar followed by a newline
  *
@@ -8,7 +9,7 @@ int main (void)
 {
 
 {
-putchar(_putchar)
+_putchar('_putchar')
 }
 
 Return: 0
