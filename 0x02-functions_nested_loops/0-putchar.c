0462e93023f7e8a011f4b238f13cc70fcea82835
emrysben <emyrsben32@gmail.com>
2022-09-13 21:05:09 +0000

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
-#include "main.h"
+#include <main.h>
+
 /**
+ * - main entry point
  * - program that prints _putchar followed by a newline
  *
  * - Return: 0
  */
-int main (void)
+int main(void)
 {
 
 {
