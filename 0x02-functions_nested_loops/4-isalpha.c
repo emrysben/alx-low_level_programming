ab8377d66346c9424c4c4cdad7746d6571068a38
emrysben <emyrsben32@gmail.com>
2022-09-17 23:28:48 +0000

    4-isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -11,8 +11,10 @@
 int _isalpha(int c)
 {
 
+
+		if ((c = 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 	{
-		return ((c = 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+		return (1);
 	}
 	return (0);
 }
