abc1271831ffc29b755fe5534c79e4d622828172
emrysben <emyrsben32@gmail.com>
2022-09-17 22:31:39 +0000

    5-sign git commit -m 5-sign git commit -m 5-sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * print_sign - print the sign of a number
+ *
+ * @n: character to print
+ *
+ * Return: 1 and print + if n is greater than zero
+ * or 0 and print 0 if n is zero
+ * or -1 and print - if n is less than zero
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+
+	{
+		_putchar('+');
+	}
+	return ('1');
+
+	if (n == 0)
+
+	{
+		_putchar('0');
+	}
+	return (0);
+
+	if (n < 0)
+
+	{
+		_putchar('-');
+	}
+	return (-1);
+
+}
