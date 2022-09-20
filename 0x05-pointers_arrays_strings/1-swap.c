771317d53173bcec96e4a04a23b6345d08cc4bc2
emrysben <emyrsben32@gmail.com>
2022-09-20 03:55:55 +0000

    1-swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swao_int - swaps the value of two integers
+ *
+ * @a: first integer to swap
+ * @b: second integer to swap
+ *
+ */
+void swap__int(int *a, int *b)
+{
+	int c = *b;
+	int d = 0;
+	*a = c;
+	*b = d;
+}
