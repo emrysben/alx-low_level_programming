bbf5c84f6d76459b0d5e241280633cf5589cab9e
emrysben <emyrsben32@gmail.com>
2022-10-12 21:16:25 +0000

    task0

--- 0x0F-function_pointers/main.h
+++ 0x0F-function_pointers/main.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef FUNCTION_POINTERS_
+#define FUNCTION_POINTERS_
 
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
